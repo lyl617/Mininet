27980edd43e500bd8d6242543a0abd6bd4ff9e34
lyl617 <lyl617@mail.ustc.edu.cn>
2018-07-02 09:18:23 +0800

    mininet2

--- mymininet2.py
+++ mymininet2.py
@@ -1,33 +1,36 @@
 #!/usr/bin/python
 
-from mininet.net import Mininet 
+ 
+
+from mininet.net import Mininet
 from mininet.node import Controller
 from mininet.topo import SingleSwitchTopo
 from mininet.log import setLogLevel
 
 import os
 
-class POXBridge(Controller):
-	#Custom Controller class to invoke POX forwarding.l2_learning"
-	def start(self):
-		#start pox learning switch
-		self.pox = '%s/pox/pox.py'%os.environ['HOME']
-		self.cmd(self.pox,'forwarding.l2_learning &')
-		#self.cmd(self.pox,'forwarding.hub &')
-	def stop(self):
-		#stop pox
-		self.cmd('kill %'+self.pox)
-controllers = {'poxbridge':POXBridge}
+class POXBridge( Controller ):
+    "Custom Controller class to invoke POX forwarding.l2_learning"
+    def start ( self ):
+        #Start POX learning switch
+        self.pox = 'pox.py'
+        self.cmd( self.pox, 'forwarding.l2_learning &' )
+        #self.cmd( self.pox, 'forwarding.hub &' )
+    def stop ( self ):
+        #Stop POX"
+      self.cmd( 'kill %' + self.pox )
 
-if __name__=='__main__':
-	setLogLevel('info')
-	net = Mininet(topo=SingleSwitchTopo(2),controller=POXBridge)
-	net.start()
-	net.pingAll()
-	h1,h2 = net.get('h1','h2')
-	result1 = h1.cmd('ping -c 5 10.0.0.2')
-	print result1
-	result2 = h2.cmd('ping -c 5 10.0.0.1')
-	print result2
-	net.stop()
+controllers = { 'poxbridge': POXBridge }
 
+if __name__ == '__main__':
+    setLogLevel( 'info' )
+    net = Mininet( topo=SingleSwitchTopo( 2), controller=POXBridge )
+    net.start()
+    net.pingAll()
+    h1, h2 = net.get( 'h1', 'h2' )
+    #net.iperf((h1, h2))
+    result1=h1.cmd('ping -c 5 10.0.0.2')
+    print result1
+    result2=h2.cmd('ping -c 5 10.0.0.1')
+    print result2
+    net.stop()
--- pox.py
+++ pox.py
@@ -88,93 +88,104 @@ class LearningSwitch(object):
   	#log.debug("Initializing LearningSwitch, transparent=%s",str(self.transparent)))
   	#
   	
-  def _handle_PacketIn(self,event):
-  	"""
-  	Handle packet in messages from the switch to implement above algorithm
-  	"""
-  	packet =  event.parsed
-    def flood(message = None):
-    	"""Floods the packet"""
-    	msg = of.ofp_packet_out()
-    	if time.time() - self.connection.connect_time >= _flood_delay:
-    		#only flood if we've been connected for a little while...
-    		if not self.hold_down_expired:
-    			self.hold_down_expired = True
-    			log.info("%s:Flood hold_down expored --flooding",dpid_to_str(event.dpid))
-
-    		if message is not None:log.debug(message)
-    		#log.debug("%i: flood %s -> %s", event.dpid,packet.src,packet.dst)
+def _handle_PacketIn (self, event):
+    """
+    Handle packet in messages from the switch to implement above algorithm.
+    """
+    packet = event.parsed
+    def flood (message = None):
+      """ Floods the packet """
+      msg = of.ofp_packet_out()
+      if time.time() - self.connection.connect_time >= _flood_delay:
+        # Only flood if we've been connected for a little while...
+        if self.hold_down_expired is False:
+          # Oh yes it is!
+          self.hold_down_expired = True
+          log.info("%s: Flood hold-down expired -- flooding",
+              dpid_to_str(event.dpid))
+        if message is not None: log.debug(message)
+        #log.debug("%i: flood %s -> %s", event.dpid,packet.src,packet.dst)
         # OFPP_FLOOD is optional; on some switches you may need to change
-        # this to OFPP_ALL
-            msg.actions.append(of.ofp_action_output(port = of.OFPP_FLOOD))
-        else:
-        	pass
-        msg.data = event.ofp
-        msg.in_port = event.port
-        self.connection.send(msg)
-    def drop(duration = None):
-    	"""
+        # this to OFPP_ALL.
+        msg.actions.append(of.ofp_action_output(port = of.OFPP_FLOOD))
+      else:
+        pass
+        #log.info("Holding down flood for %s", dpid_to_str(event.dpid))
+      msg.data = event.ofp
+      msg.in_port = event.port
+      self.connection.send(msg)
+    def drop (duration = None):
+      """
       Drops this packet and optionally installs a flow to continue
       dropping similar ones for a while
-      """
+            """
       if duration is not None:
-      	 if not isinstance(duration,tuple):
-      	 	duration = (duration,duration)
-      	 msg = of.ofp_flow_mod()
-      	 msg.match = of.ofp_match.from_packet(packet)
-      	 msg.idle_timeout = duration[0]
-      	 msg.hard_timeout = duration[1]
-      	 msg.buffer_id = event.ofp.buffer_id
-      	 self.connection.send(msg)
+        if not isinstance(duration, tuple):
+          duration = (duration,duration)
+        msg = of.ofp_flow_mod()
+        msg.match = of.ofp_match.from_packet(packet)
+        msg.idle_timeout = duration[0]
+        msg.hard_timeout = duration[1]
+        msg.buffer_id = event.ofp.buffer_id
+        self.connection.send(msg)
       elif event.ofp.buffer_id is not None:
-      	msg = of.ofp_packet_out()
-      	msg.buffer_id = event.ofp.buffer_id
-      	msg.in_port = event.port
-      	self.connection.send(msg)
-    self.macToPort[packet.src] = event.port
+        msg = of.ofp_packet_out()
+        msg.buffer_id = event.ofp.buffer_id
+        msg.in_port = event.port
+        self.connection.send(msg)
+    self.macToPort[packet.src] = event.port # 1
 
-    if not self.transparent:
-    	if packet.type == packet.LLDP_TYPE or packet.dst.isBridgeFiltered():
-    		drop()
-    		return
+    if not self.transparent: # 2
+      if packet.type == packet.LLDP_TYPE or packet.dst.isBridgeFiltered():
+        drop() # 2a
+        return
     if packet.dst.is_multicast:
-    	flood()
+      flood() # 3a
     else:
-    	if packet.dst not in self.macToPort:
-    		flood("Port for %s unknown -- flooding"%(packet.dist,))
-  		else:
-  			port = self.macToPort[packet.dst]
-  			if port == event.port:
-  				log.warning("Same port for packet from %s->%s on %s.%s. Drop.")%(packet.src,packet.dst,)
-  				drop(10)
-  				return
-  		log.debug("installing flow for %s.%i->%s.%i"%(packet.src,event.port,packet.dst,port))
-  		msg = of.ofp_flow_mod()#添加流表项
-  		msg.match = of.ofp_match.from_packet(packet,event.port)
-  		msg.idle_timeout = 10
-  		msg.hard_timeout = 30
-  		msg.actions.append(of.ofp_action_output(port = port))
-  		msg.data = event.ofp
-  		self.connection.send(msg)
-class l2_learning(object):
-	"""
-	waits for openflow switches to connect and makes them learning switches
-	"""
-	def __init__(self,transparent):
-		core.openflow.addListeners(self)
-		self.transparent = transparent
-	def _handle_ConnectionUp(self,event):
-		log.debug("Connection %s"% (event.connection))
-		LearningSwitch(event.connection,self.transparent)
-def launch(transparent = False,hold_down = _flood_delay):
-	"""
-	Starts an L2 learning switch
-	"""
-	try:
-		global _flood_delay
-		_flood_delay = int(str(hold_down),10)
-		assert _flood_delay >= 0
-	except:
-		raise RuntimeError("Expected hold-down to be a number")
-core.registerNew(l2_learning,str_to_bool(transparent))
+      if packet.dst not in self.macToPort: # 4
+        flood("Port for %s unknown -- flooding" % (packet.dst,)) # 4a
+      else:
+        port = self.macToPort[packet.dst]
+        if port == event.port: # 5
+          # 5a
+          log.warning("Same port for packet from %s -> %s on %s.%s.  Drop."
+              % (packet.src, packet.dst, dpid_to_str(event.dpid), port))
+          drop(10)
+          return
 
+        # 6
+        log.debug("installing flow for %s.%i -> %s.%i" %
+                  (packet.src, event.port, packet.dst, port))
+        msg = of.ofp_flow_mod()
+        msg.match = of.ofp_match.from_packet(packet, event.port)
+        msg.idle_timeout = 10
+        msg.hard_timeout = 30
+        msg.actions.append(of.ofp_action_output(port = port))
+        msg.data = event.ofp # 6a
+        self.connection.send(msg)
+
+class l2_learning (object):
+  """
+ Waits for OpenFlow switches to connect and makes them learning switches.
+  """
+  def __init__ (self, transparent):
+    core.openflow.addListeners(self)
+    self.transparent = transparent
+  def _handle_ConnectionUp (self, event):
+    log.debug("Connection %s" % (event.connection,))
+
+    LearningSwitch(event.connection, self.transparent)
+
+def launch (transparent=False, hold_down=_flood_delay):
+  """
+  Starts an L2 learning switch.
+  """
+  try:
+    global _flood_delay
+    _flood_delay = int(str(hold_down), 10)
+    assert _flood_delay >= 0
+  except:
+    raise RuntimeError("Expected hold-down to be a number")
+  core.registerNew(l2_learning, str_to_bool(transparent))
+
+ 
